83db00fcd273a3afb33cb22d6d0c685b9eb0778c
Justin Collins <justin@presidentbeef.com>
2014-05-07 09:12:18 -0700

    Support Rails 4 model queries in CheckRedirect
    as mentioned in #486

--- lib/brakeman/checks/check_redirect.rb
+++ lib/brakeman/checks/check_redirect.rb
@@ -19,6 +19,10 @@ class Brakeman::CheckRedirect < Brakeman::BaseCheck
       @model_find_calls.merge [:from, :group, :having, :joins, :lock, :order, :reorder, :select, :where]
     end
 
+    if @tracker.config[:rails_version] >= "4.0.0"
+      @model_find_calls.merge [:find_by, :find_by!, :take]
+    end
+
     @tracker.find_call(:target => false, :method => :redirect_to).each do |res|
       process_result res
     end
